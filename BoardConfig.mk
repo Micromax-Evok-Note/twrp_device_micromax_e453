440221052c28d9804f3f5d734ae1a04701461949
Sooraj S Prakash <soorajsprakash007@gmail.com>
2019-12-23 21:51:47 +0530

    First commit

---
+++ BoardConfig.mk
@@ -0,0 +1,60 @@
+#
+# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/Micromax/E453
+
+TARGET_BOARD_PLATFORM := mt6753
+TARGET_BOOTLOADER_BOARD_NAME := E453
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 18156032
+BOARD_FLASH_BLOCK_SIZE := 0
+BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_RECOVERY_SWIPE := true
+BOARD_USES_MMCUTILS := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_INCLUDE_CRYPTO := true
+include $(LOCAL_PATH)/kernel.mk
+include device/generic/twrpbuilder/BoardConfig64.mk
+
+# Architecture
+ifeq ($(FORCE_32_BIT),true)
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+else
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
+TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
+TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
+endif
